4c7e35d4c732f92d7bf00baeca9949fa004d7230
CarlinReny <96654645+CarlinReny@users.noreply.github.com>
2021-12-25 21:02:07 +0530

    Add files via upload

---
+++ Fibonacciseries.java
@@ -0,0 +1,42 @@
+package week1.day1;
+
+
+public class Fibonacciseries {
+
+
+
+	public static void main(String[] args) {
+
+		int Firstnumber=0;
+		int secondnumber = 1;		
+		int range=8;
+		int sum=0;
+
+		System.out.println(Firstnumber);
+		
+		for (Firstnumber = 1; Firstnumber <=range; Firstnumber++) {	
+			
+			Firstnumber=secondnumber;
+
+			secondnumber=sum;
+
+			sum=Firstnumber+secondnumber;			
+
+			System.out.println(sum);
+
+		}
+
+
+
+
+
+
+
+	}
+
+
+}
+
+
+
+
